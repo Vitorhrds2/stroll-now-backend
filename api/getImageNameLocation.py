66e19dda51b62329ba1a0d343e86df7e38bc06ec
Vitorhrds2 <vitor.santos@sou.inteli.edu.br>
2024-02-03 22:50:24 -0300

    add backend files

--- api/getImageNameLocation.py
+++ api/getImageNameLocation.py
@@ -6,13 +6,43 @@ from html import unescape
 from pprint import pprint
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from getWebsiteCoordsNameLocation import encontrar_urls_no_json
 import getInstaOnWebsite
 from concurrent.futures import ThreadPoolExecutor
 from scrapping import baixar_posts, pegarBio
 
 app = Flask(__name__)
 CORS(app, origins="*")
+
+def extrair_latitude_longitude_nome(url):
+    pattern = r'url\?q=(.*?)&opi=.*?,null,\[null,null,(-?\d+\.\d+),(-?\d+\.\d+)\],"[^"]+","(.*?)"'
+    matches = re.findall(pattern, url)
+
+    informacoes = []
+    for match in matches:
+        website, latitude, longitude, nome = match
+        informacoes.append({"website": website, "latitude": latitude, "longitude": longitude, "nome": nome})
+
+    return informacoes
+
+
+def encontrar_urls_no_json(data):
+    urls_encontradas = []
+
+    def buscar_recursivamente(item):
+        if isinstance(item, list):
+            for subitem in item:
+                buscar_recursivamente(subitem)
+
+        elif isinstance(item, str) and '/url?q=' in item:
+            informacoes = extrair_latitude_longitude_nome(item)
+
+            if informacoes:
+                for info in informacoes:
+                    urls_encontradas.append(info)
+
+    buscar_recursivamente(data)
+
+    return urls_encontradas
 
 def encontrar_textos_entre_lista(lista, texto_inicio, texto_fim):
     textos_entre = []
