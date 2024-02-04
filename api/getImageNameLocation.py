87d97c0a0f57426e11725d33556ced9cf910e22b
Vitorhrds2 <vitor.santos@sou.inteli.edu.br>
2024-02-04 12:01:25 -0300

    Route to locals that haven't websites fixed

--- api/getImageNameLocation.py
+++ api/getImageNameLocation.py
@@ -1,59 +1,73 @@
 import requests
 import json
-import re
 import urllib.parse
 from html import unescape
 from pprint import pprint
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from concurrent.futures import ThreadPoolExecutor
-from bs4 import BeautifulSoup
 import instaloader
+from bs4 import BeautifulSoup
+import re
 
 app = Flask(__name__)
 CORS(app, origins="*")
 
+def modificar_largura_altura_url(url, nova_largura=1178, nova_altura=1138):
+    return re.sub(r'&[wh]=\d+', f'&w={nova_largura}&h={nova_altura}', url)
 
-def baixar_posts(profile):
-    bot = instaloader.Instaloader()
-    profile = instaloader.Profile.from_username(bot.context, profile)
-    posts = profile.get_posts()
-    numero_de_posts_desejado = 1
-    posts_baixados = 0
 
-    for index, post in enumerate(posts, 1):
-        if posts_baixados >= numero_de_posts_desejado:
-            break
+def extrair_latitude_longitude_nome_da_url_imagem(url, nomes_vistos):
+    pattern = r'/p/AF1Qip(.*?)=w.*?\",\"(.*?)\",.*?,(-?\d+\.\d+),(-?\d+\.\d+)'
+    pattern_street = r'/v1/thumbnail\?panoid=(.*?)&pitch=0&thumbfov=100.*?\",\"(.*?)\",.*?,(-?\d+\.\d+),(-?\d+\.\d+)'
+    matches = re.findall(pattern, url)
+    matches_street = re.findall(pattern_street, url)
 
-        posts_baixados += 1
-        return post.url
+    informacoes = []
 
-def pegarBio(profile):
-    bot = instaloader.Instaloader()
-    profile = instaloader.Profile.from_username(bot.context, profile)
-    return profile.biography
+    for match in matches:
+        url_id, name, longitude, latitude = match
 
-def search_instagram_on_website(url):
-    try:
-        headers = {
-            'authority': 'www.google.com',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-            'accept-language': 'en-US,en;q=0.9',
-            'cache-control': 'max-age=0',
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
-        }
-        response = requests.get(url, headers=headers)
+        if url_id and name and name not in nomes_vistos and "fotos" not in name:
+            nomes_vistos.add(name)
+            link_imagem = f"https://lh5.googleusercontent.com/p/AF1Qip{url_id}=s1031"
+            informacoes.append({"nome": name, "imagem": link_imagem, "latitude": latitude, "longitude": longitude})
+    
+    for match_street in matches_street:
+        url_id, name, longitude, latitude = match_street
 
-        response.raise_for_status()
+        if url_id and name and name not in nomes_vistos and "fotos" not in name:
+            nomes_vistos.add(name)
+            url_id = modificar_largura_altura_url(url_id, nova_largura=1178, nova_altura=1138)
+            link_imagem = f"https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid={url_id}"
+            informacoes.append({"nome": name, "imagem": link_imagem, "latitude": latitude, "longitude": longitude})
 
-        soup = BeautifulSoup(response.text, 'html.parser')
+    return informacoes
 
-        instagram_tag = soup.find('a', href=lambda href: href and 'instagram.com' in href)
-        if instagram_tag:
-            return instagram_tag['href']
-    except Exception as e:
-        print(e)
-        return None
+def encontrar_urls_no_json_image(data):
+    urls_encontradas = []
+
+    def buscar_recursivamente(item):
+        if isinstance(item, list):
+            for subitem in item:
+                buscar_recursivamente(subitem)
+
+        elif isinstance(item, str) and '/p/AF1Qip' in item:
+            nomes_vistos = set()
+            info = extrair_latitude_longitude_nome_da_url_imagem(item, nomes_vistos)
+            if info:
+                for info in info:
+                    urls_encontradas.append(info)
+        
+        elif isinstance(item, str) and 'streetviewpixels-pa.googleapis.com' in item:
+            info = extrair_latitude_longitude_nome_da_url_imagem(item)
+            if info:
+                for info in info:
+                    urls_encontradas.append(info)
+
+    buscar_recursivamente(data)
+
+    return urls_encontradas
 
 def extrair_latitude_longitude_nome(url):
     pattern = r'url\?q=(.*?)&opi=.*?,null,\[null,null,(-?\d+\.\d+),(-?\d+\.\d+)\],"[^"]+","(.*?)"'
@@ -86,52 +100,60 @@ def encontrar_urls_no_json(data):
 
     return urls_encontradas
 
-def encontrar_textos_entre_lista(lista, texto_inicio, texto_fim):
-    textos_entre = []
-    for item in lista:
-        if isinstance(item, str):
-            posicao_inicio = 0
-            while True:
-                posicao_inicio = item.find(texto_inicio, posicao_inicio)
-                if posicao_inicio == -1:
-                    break
-                posicao_fim = item.find(texto_fim, posicao_inicio + len(texto_inicio))
-                if posicao_fim == -1:
-                    break
-                texto_entre = item[posicao_inicio + len(texto_inicio):posicao_fim]
-                textos_entre.append(texto_entre)
-                posicao_inicio = posicao_fim + len(texto_fim)
-    return textos_entre
+def search_instagram_on_website(url):
+    try:
+        headers = {
+            'authority': 'www.google.com',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+        }
+        response = requests.get(url, headers=headers)
 
-def modificar_largura_altura_url(url, nova_largura=1178, nova_altura=1138):
-    return re.sub(r'&[wh]=\d+', f'&w={nova_largura}&h={nova_altura}', url)
+        response.raise_for_status()
 
-def processar_url(url, nomes_vistos):
-    url = url.replace("\"", "")
-    url = re.sub(r'w203.*?,', 's1031,', url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    url, name = url.split(",", 1)
-    if url and name and name not in nomes_vistos and "fotos" not in name and "Photos" not in name:
-        nomes_vistos.add(name)
+        instagram_tag = soup.find('a', href=lambda href: href and 'instagram.com' in href)
+        if instagram_tag:
+            return instagram_tag['href']
+    except Exception as e:
+        print(e)
+        return None
 
-        if "pitch=0&thumbfov=100" in url:
-            url = modificar_largura_altura_url(url, nova_largura=1178, nova_altura=1138)
-            link_imagem = f"https://streetviewpixels-pa.googleapis.com/v1/thumbnail?panoid={url}"
-        else:
-            link_imagem = f"https://lh5.googleusercontent.com/p/AF1Qip{url}"
+def baixar_posts(profile):
+    bot = instaloader.Instaloader()
+    profile = instaloader.Profile.from_username(bot.context, profile)
+    posts = profile.get_posts()
+    numero_de_posts_desejado = 1
+    posts_baixados = 0
 
-        return {"nome": name, "imagem": link_imagem}
-    return None
+    for index, post in enumerate(posts, 1):
+        if posts_baixados >= numero_de_posts_desejado:
+            break
+
+        posts_baixados += 1
+        return post.url
+
+def pegarBio(profile):
+    bot = instaloader.Instaloader()
+    profile = instaloader.Profile.from_username(bot.context, profile)
+    return profile.biography
 
 def juntar_listas_por_nome(locais_info, websites_info):
     locais_dict = {item['nome']: item for item in locais_info}
     for website_info in websites_info:
         nome = website_info['nome']
+
         if nome in locais_dict:
             locais_dict[nome].update(website_info)
+
         else:
             locais_dict[nome] = website_info
+            
     return list(locais_dict.values())
+
 
 def obter_party_banner(item):
     website = item.get('website')
@@ -139,19 +161,23 @@ def obter_party_banner(item):
         if website and 'instagram' in website:
             website = urllib.parse.unquote(website)
             perfil = website.split('.com/')[-1].split('/')[0].split('%')[0].split('?')[0]
-            item['party_banner'] = baixar_posts(perfil)  # Cria uma lista com o URL do banner de festa
+            item['party_banner'] = baixar_posts(perfil)  
             item['bio_casa_noturna'] = pegarBio(perfil)
             pprint("Perfil no Instagram buscado diretamente: " + perfil)
+
         elif website and 'instagram' not in website:
             website = urllib.parse.unquote(website)
             instagram_extracted = search_instagram_on_website(website)
+
             if instagram_extracted:
                 profile = instagram_extracted.split('.com/')[-1].split('/')[0].split('%')[0].split('?')[0]
-                item['party_banner'] = baixar_posts(profile)  # Cria uma lista com o URL do banner de festa
+                item['party_banner'] = baixar_posts(profile)  
                 item['bio_casa_noturna'] = pegarBio(profile)
                 print("Perfil no Instagram buscado no website:", profile, "Website:", website)
+
             else:
                 print("Nenhum link de Instagram encontrado no website:", website)
+
     except Exception as e:
         print("Erro ao buscar Instagram no website:", e)
 
@@ -172,18 +198,7 @@ def get_location_image():
 
         try:
             data = json.loads(js)
-            locais_info = []
-
-            for item in data:
-                if isinstance(item, list):
-                    textos_entre = encontrar_textos_entre_lista(item, "/p/AF1Qip", "\",[")
-                    textos_entre_street = encontrar_textos_entre_lista(item, "/v1/thumbnail?panoid=", "\",null")
-
-                    if textos_entre or textos_entre_street:
-                        for url in textos_entre + textos_entre_street:
-                            info = processar_url(url, nomes_vistos)
-                            if info:
-                                locais_info.append(info)
+            locais_info = encontrar_urls_no_json_image(data)
 
             websites_info = encontrar_urls_no_json(data)
             locais_websites_combinados = juntar_listas_por_nome(locais_info, websites_info)
@@ -191,7 +206,7 @@ def get_location_image():
             print("-" * 100)
 
             if locais_websites_combinados:
-                # Utilizando ThreadPoolExecutor para obter party_banner concorrentemente
+                
                 with ThreadPoolExecutor(max_workers=len(locais_websites_combinados)) as executor:
                     executor.map(obter_party_banner, locais_websites_combinados)
 
